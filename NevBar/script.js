7d94fa2c39dfac216f024604a0a70c28aff45b88
Abdul Muizz <abdulmuizz.ads@gmail.com>
2025-11-09 13:10:53 +0500

    Menu Gsap

---
+++ NevBar/script.js
@@ -0,0 +1,29 @@
+var menu = document.querySelector("#nev i");
+var cross = document.querySelector("#full i");
+var tl = gsap.timeline();
+
+tl.to("#full", {
+  right: 0,
+  duration: 0.8,
+});
+
+tl.from("#full h4", {
+  x: 150,
+  duration: 0.7,
+  stagger: 0.25,
+  opacity: 0,
+});
+
+tl.from("#full i", {
+  opacity: 0,
+});
+
+tl.pause();
+
+menu.addEventListener("click", () => {
+  tl.play();
+});
+
+cross.addEventListener("click", () => {
+  tl.reverse();
+});
